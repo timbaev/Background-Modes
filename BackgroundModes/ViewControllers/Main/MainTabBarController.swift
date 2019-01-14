bd9eb681f6e1b04933253670ea9b278756afa536
TimurShafigullin <timur.shafigullin@flatstack.com>
2019-01-14 10:11:18 +0300

    Setup project

---
+++ BackgroundModes/ViewControllers/Main/MainTabBarController.swift
@@ -0,0 +1,13 @@
+//
+//  MainTabBarController.swift
+//  BackgroundModes
+//
+//  Created by Timur Shafigullin on 14/01/2019.
+//  Copyright © 2019 Timur Shafigullin. All rights reserved.
+//
+
+import UIKit
+
+class MainTabBarController: UITabBarController {
+    
+}
